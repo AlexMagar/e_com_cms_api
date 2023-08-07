d0a2d37e3775f99d7994aa9b53f9d31a0d1d0471
AlexMagar <lx.mgr07@gmail.com>
2023-08-07 22:57:23 +1000

    created schema for payment option

---
+++ src/modles/payment-option/paymentOptionSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const paymentOptionSchema = new mongoose.Schema({
+    status:{
+        type: String,
+        default: 'inactive'
+    },
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    }
+},
+{
+    timestamps: true
+}
+)
+
+export default mongoose.model("PaymentOption", paymentOptionSchema) //this will make paymentOptions
